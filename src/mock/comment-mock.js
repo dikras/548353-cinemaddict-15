4c7b3a74f9fef388b6fad80bd52f4e5a1a285874
Dmitry Krasyukov <dmi1517@yandex.ru>
2021-08-09 10:48:41 +0300

    Революция или эволюция? (часть 1)

--- src/mock/comment-mock.js
+++ src/mock/comment-mock.js
@@ -1,12 +1,10 @@
 import { COMMENT_EMOTIONS } from '../const.js';
 import { getRandomItem } from '../utils.js';
 
-const generateComment = () => ({
+export const generateComment = () => ({
   id: '42',
   author: 'Ilya O\'Reilly',
   comment: 'a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.',
   date: '2019-05-11T16:12:32.554Z',
   emotion: getRandomItem(COMMENT_EMOTIONS),
 });
-
-export { generateComment };
--- src/view/films-count.js
+++ src/view/films-count.js
@@ -1,3 +1,26 @@
-import { MOVIE_CARDS_COUNT } from '../const.js';
+import { CardCount } from '../const.js';
+import { createElement } from '../utils.js';
 
-export const createFilmsCountTemplate = () => (`<p>${MOVIE_CARDS_COUNT} movies inside</p>`);
+const createFilmsCountTemplate = () => (`<p>${CardCount.TOTAL} movies inside</p>`);
+
+export default class FilmsCount {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createFilmsCountTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
